88ffdfbbb462ab10ad5825be9bb17e2f3cdb77cf
Nikhilkumar Italiya <nikhilkumar.italiya@st.ovgu.de>
2020-05-28 15:59:50 +0200

    added missing files

---
+++ app/src/main/java/com/ss2020/project/demorpher/ViewPhotos.java
@@ -0,0 +1,32 @@
+package com.ss2020.project.demorpher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Environment;
+import android.widget.ImageView;
+
+
+import org.jmrtd.lds.icao.MRZInfo;
+
+
+import java.io.File;
+
+public class ViewPhotos extends AppCompatActivity {
+
+    ImageView photos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_photos);
+
+        photos = (ImageView) findViewById(R.id.photos);
+
+        //temporary
+        Bitmap image = BitmapFactory.decodeFile(getExternalFilesDir(Environment.DIRECTORY_DCIM) + File.separator  + "photo.jpeg");
+        photos.setImageBitmap(image);
+    }
+}
